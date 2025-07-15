68e8fd2943d474e0fe167959cac346cd75ac5d0e
TrucLam <httlam2004ntkt@gmail.com>
2025-07-15 16:53:32 +0700

    Refactor Arrow component and update Header, Auth, and Introduction styles for improved responsiveness: Simplify Arrow component structure, adjust mobile menu width in Header, and enhance Auth and Introduction styles with new media queries for better layout across various screen sizes.

--- src/assets/Icons/Arrow.tsx
+++ src/assets/Icons/Arrow.tsx
@@ -1,22 +1,20 @@
 const Arrow = () => {
   return (
-    <div>
-      <svg
-        width="25"
-        height="22"
-        viewBox="0 0 25 22"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M2 20.5L23 1.5M23 1.5V18M23 1.5H5"
-          stroke="#FF9D06"
-          stroke-width="3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    </div>
+    <svg
+      width="25"
+      height="22"
+      viewBox="0 0 25 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 20.5L23 1.5M23 1.5V18M23 1.5H5"
+        stroke="#FF9D06"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
   );
 };
 
