69d4a97ae442056f914ce6bbde49e847dbcf6c08
TrucLam <httlam2004ntkt@gmail.com>
2025-07-16 13:09:00 +0700

    Comment out additional help links in Header component to streamline UI: Temporarily disable help links in the Header component, maintaining structure for potential future reintroduction while enhancing overall user experience.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -159,9 +159,6 @@ const Header = () => {
                     </div>
                   </div>
                   <div className={styles["Header-popover-menu"]}>
-                    {/* <Link to="/" className={styles["Header-popover-item"]}>
-                      Trợ giúp
-                    </Link> */}
                     <button
                       className={styles["Header-popover-item"]}
                       onClick={handleSignOut}
@@ -199,9 +196,6 @@ const Header = () => {
               >
                 <div className={styles["Header-notAuth-content"]}>
                   <div className={styles["Header-notAuth-menu"]}>
-                    {/* <Link to="/" className={styles["Header-notAuth-item"]}>
-                      <Contact /> Trợ giúp
-                    </Link> */}
                     <button
                       className={styles["Header-notAuth-item"]}
                       onClick={() => {
@@ -301,16 +295,7 @@ const Header = () => {
                 />{" "}
                 Danh sách yêu thích
               </Link>
-              {/* <Link
-                to="/"
-                className={`${styles["Header-mobile-menu-item"]} ${
-                  isActive("#") ? styles["Header-mobile-menu-item--active"] : ""
-                }`}
-                onClick={closeMobileMenu}
-                data-tooltip="Trợ giúp"
-              >
-                <Contact /> Trợ giúp
-              </Link> */}
+
               {user ? (
                 <button
                   className={`${styles["Header-mobile-menu-item"]} ${
